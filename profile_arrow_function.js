02d54356363eba85996b659d68a6dcbe2b7e12bc
Dimasadhy <113024825+Dimasadhy@users.noreply.github.com>
2023-10-02 13:46:52 +0700

    Add files via upload

---
+++ profile_arrow_function.js
@@ -0,0 +1,6 @@
+const login = (namaDepan, namaBelakang, email ) => {
+  console.log("Login Screen " + namaDepan + " " + namaBelakang)
+  console.log("email " + email);
+}
+
+login("dimas", "adhyaksa", "dimasadhy07@gmail.com")
---
+++ profile_reg_function.js
@@ -0,0 +1,7 @@
+function home(namaDepan, namaBelakang, email) {
+  console.log("Halo " + namaDepan +" " + namaBelakang);
+  console.log("email " + email);
+  console.log("Selamat Datang di Home Screen");
+}
+
+home("dimas", "adhyaksa", "dimasadhy07@gmail.com");
